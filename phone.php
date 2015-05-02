c1edeb502fdfc7b49c7ceb65f775f5a1bd69a6bf
no-glue <nikolapav1985@gmail.com>
2015-05-02 20:32:50 +0200

    phone

---
+++ phone.php
@@ -0,0 +1,13 @@
+<?php
+class Phone {
+  public function __construct() {
+  }
+  public function setPhone($phone) {
+    $this->phone = $phone;
+  }
+  public function getPhone($) {
+    return $this->phone;
+  }
+  public function convert() {
+  }
+};
